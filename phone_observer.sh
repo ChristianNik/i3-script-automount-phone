cd03d1cb03859f23628c1fcc43107c40497b22fe
Christian <chnikel@gmail.com>
2021-11-30 09:59:03 +0100

    change icon psacing

--- phone_observer.sh
+++ phone_observer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 function print_offline {
-    echo "%{F#555555}    %{F-}"
+    echo "%{F#555555}  %{F-}"
 }
 
 function print_online {
-    echo "%{A1:(scrcpy -S >/dev/null 2>&1 &):} %{F#fff}  %{F-} %{A}"
+    echo "%{A1:(scrcpy -S >/dev/null 2>&1 &):} %{F#fff}%{F-} %{A}"
 }
 
 function print_connected {
-    echo "%{A1:i3-msg '[class="scrcpy"] focus':} %{F#5294E2}  %{F-} %{A}"
+    echo "%{A1:i3-msg '[class="scrcpy"] focus':} %{F#5294E2}%{F-} %{A}"
 }
 function main() {
     is_running=$(pgrep scrcpy | wc -l)
